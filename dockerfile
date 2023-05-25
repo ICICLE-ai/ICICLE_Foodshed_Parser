6340789e18e4391f2b0816f73a0ef7ba23aefe3f
guzman109 <guzman.109@osu.edu>
2023-05-25 18:24:15 -0400

    Added metrics to application

--- dockerfile
+++ dockerfile
@@ -1,45 +1,50 @@
 
 FROM nginx:1.21.3
 
+# Set environment variables
 ENV PIP_DISABLE_PIP_VERSION_CHECK=on \
-  PIP_DEFAULT_TIMEOUT=100 
+  PIP_DEFAULT_TIMEOUT=100 \
+  PYTHONUNBUFFERED=1 \
+  PYTHONHASHSEED=random \
+  PROMETHEUS_MULTIPROC_DIR=/tmp/prometheus
 
+# Install system dependencies
 RUN apt update && apt install -y python3.7 python3-venv python3-pip git
-# RUN curl -sSL https://install.python-poetry.org | python3 -
 
 WORKDIR /app
-# Install poetry environment
-# RUN python -m venv venv
-RUN python3.7 -m pip install --upgrade pip
-RUN python3.7 -m pip install --upgrade setuptools
-RUN python3.7 -m pip install --upgrade wheel
 
+# Upgrade pip, setuptools and wheel. Need this to avoid errors installing older packages
+RUN python3.7 -m pip install --upgrade pip \
+  setuptools \
+  wheel
 
+# Install dependencies for Conversational AI
 COPY requirements.txt ./requirements.txt
 RUN python3.7 -m pip install -r requirements.txt
 RUN python3.7 -m pip install git+https://github.com/microsoft/task_oriented_dialogue_as_dataflow_synthesis.git
-# RUN python3.7 -m pip install pandas
 
+# Install dependencies for application
 RUN python3.7 -m pip install fastapi \
-    prometheus-client \
-    uvicorn \
-    gunicorn
+  prometheus-client \
+  uvicorn \
+  gunicorn
 
-ENV PYTHONUNBUFFERED=1 \
-  PYTHONHASHSEED=random
-# RUN apt update && apt install -y  python3-venv
+# Create directory for prometheus multiprocess
+RUN mkdir -p /tmp/prometheus
 
-WORKDIR /app
-
-# COPY --from=builder /app/venv ./venv
+# Copy source files
 COPY src/ ./
 COPY main.py ./main.py
 COPY demo.py ./demo.py
 COPY runCompletePipeline.sh ./runCompletePipeline.sh
 COPY trained_models/ ./trained_models
-COPY docker-entrypoint.sh ./docker-entrypoint.sh
+
+# Copy nginx and gunicorn configuration files
 COPY gunicorn.conf.py ./gunicorn.conf.py
 COPY nginx.conf /etc/nginx/nginx.conf
 
-EXPOSE 80
+# Copy docker entrypoint
+COPY docker-entrypoint.sh ./docker-entrypoint.sh
+
+# Run the application
 ENTRYPOINT sh docker-entrypoint.sh
--- main.py
+++ main.py
@@ -17,20 +17,20 @@ import semantic_parsing_with_constrained_lm.run_instant as run_instant
 from demo import canonToCmd
 import sys, os
 
-# if "PROMETHEUS_MULTIPROC_DIR" not in os.environ:
-#     raise ValueError("PROMETHEUS_MULTIPROC_DIR must be set to existing empty dir.")
+if "PROMETHEUS_MULTIPROC_DIR" not in os.environ:
+    raise ValueError("PROMETHEUS_MULTIPROC_DIR must be set to existing empty dir.")
 
 # Start the Prometheus client.
-# start_http_server(8001)
+start_http_server(8001)
 
 # Define the Prometheus metrics.
-# METRICS_COUNTER = Counter("metrics_counter", "Number of metrics requests")
-# PING_COUNTER = Counter("ping_counter", "Number of ping requests")
-# QUERY_COUNTER = Counter("query_counter", "Number of query requests")
+METRICS_COUNTER = Counter("metrics_counter", "Number of metrics requests")
+PING_COUNTER = Counter("ping_counter", "Number of ping requests")
+QUERY_COUNTER = Counter("query_counter", "Number of query requests")
 
-# METRICS_TIME = Summary("metrics_time", "Time spent processing metrics")
-# QUERY_TIME = Summary("query_time", "Time spent processing query")
-# PING_TIME = Summary("ping_time", "Time spent processing ping")
+METRICS_TIME = Summary("metrics_time", "Time spent processing metrics")
+QUERY_TIME = Summary("query_time", "Time spent processing query")
+PING_TIME = Summary("ping_time", "Time spent processing ping")
 
 
 app = FastAPI(debug=False)
@@ -40,8 +40,8 @@ def query(q: str):
     sys.stdout = open(os.devnull, 'w') # Suppress print statements from run_instant
 
     # Increment the query counter and time the query.
-    # QUERY_COUNTER.inc()
-    # QUERY_TIME.observe(1)
+    QUERY_COUNTER.inc()
+    QUERY_TIME.observe(1)
 
     # Run the model and return the output.
     q = ' '.join(q.split('_'))
@@ -55,24 +55,24 @@ def query(q: str):
 @app.get("/ping")
 def ping():
     # Increment the ping counter and time the ping.
-    # PING_COUNTER.inc()
-    # PING_TIME.observe(1)
+    PING_COUNTER.inc()
+    PING_TIME.observe(1)
 
     # Test connection without calling the model.
     return JSONResponse(content=jsonable_encoder({'pong': True}))
 
-# @app.get("/metrics")
-# def make_metrics():
-#     # Increment the metrics counter and time the metrics.
-#     # METRICS_COUNTER.inc()
-#     # METRICS_TIME.observe(1)
+@app.get("/metrics")
+def make_metrics():
+    # Increment the metrics counter and time the metrics.
+    METRICS_COUNTER.inc()
+    METRICS_TIME.observe(1)
 
-#     # Return the metrics.
-#     registry = CollectorRegistry()
-#     multiprocess.MultiProcessCollector(registry)
-#     # resp = JSONResponse(content=jsonable_encoder(generate_latest(registry)))
-#     # return Response(prometheus_client.generate_latest())
-#     return make_asgi_app(registry)
+    # Return the metrics.
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+    # resp = JSONResponse(content=jsonable_encoder(generate_latest(registry)))
+    # return Response(prometheus_client.generate_latest())
+    return make_asgi_app(registry)
 
-# metrics = make_metrics()
-# app.mount("/metrics", metrics)
+metrics = make_metrics()
+app.mount("/metrics", metrics)
